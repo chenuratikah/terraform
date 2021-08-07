1c021aaeef2cfacbc28af5e4d00d0a2b9657a9a5
mong220394 <mong220394@users.noreply.github.com>
2021-08-07 16:05:53 +0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+# All common variables are declared here
+variable project {}
+variable region {}
+variable env {}
+variable location {
+  default     = "asia-southeast1"
+  description = "location for resources"
+}
